0fe74baa34ac291b5b2885cf80e906c4aec9d94e
roodi <igorrud95@gmail.com>
2023-10-21 18:54:10 +0700

    added create links page

--- src/components/CreatePage/CreatePage.js
+++ src/components/CreatePage/CreatePage.js
@@ -1,27 +1,69 @@
 import React, {useState} from 'react'
 import styles from './styles.module.css'
-import {MdClose,MdAdd} from 'react-icons/md';
+import {MdAdd, MdClose} from 'react-icons/md';
+import {v1} from 'uuid';
+import {useNavigate} from 'react-router-dom'
+import {fb} from './../../service'
+import {useAuth} from '../../hooks';
 
 export const CreatePage = () => {
 
     const [namePage, setNamePage] = useState('')
     const [pageLinks, setPageLinks] = useState([])
-    const [currentLink, setCurrentLink] = useState('')
+    const [currentLink, setCurrentLink] = useState({value: '', name: '', id: ''})
+    const navigate = useNavigate();
 
-    const onChangeNameHandler = (e) => setNamePage(e.target.value)
-    const onChangeLinkHandler = (e) => setCurrentLink(e.target.value)
+    const {authUser} = useAuth()
+
+    const onChangePageNameHandler = (e) => setNamePage(e.target.value)
+    const onChangeLinkNameHandler = (e) => {
+        setCurrentLink({...currentLink, name: e.target.value})
+    }
+    const onChangeLinkURLHandler = (e) => {
+        setCurrentLink({...currentLink, value: e.target.value.trim()})
+    }
     const onDeleteLinkHandler = (l) => setPageLinks(pageLinks.filter(pl => pl.id !== l.id))
+
+    const onAddHandler = () => {
+        setPageLinks([...pageLinks, {name: currentLink.name, value: currentLink.value, id: v1()}])
+        setCurrentLink({name: '', value: '', id: ''})
+    }
+
+    const createPageHandler = () => {
+        if (authUser && namePage && pageLinks.length) {
+            fb.firestore.collection('linkPages').add({
+                userId: authUser.uid,
+                links:pageLinks,
+                name: namePage
+            })
+                .then(() => {
+                    navigate(`/`);
+                })
+        }
+    }
 
     return (
         <div className={styles.main}>
             <input value={namePage}
-                   onChange={onChangeNameHandler}
-                   type="text" className={styles.name}/>
+                   onChange={onChangePageNameHandler}
+                   type="text" className={styles.name}
+                   placeholder="Page name"/>
 
             <div className={styles.links}>
-                <div>
-                    <input type="text" value={currentLink} onChange={onChangeLinkHandler}/>
-                    <div className={styles.add}><MdAdd/></div>
+                <div className={styles.linkInputs}>
+                    <input type="text"
+                           value={currentLink.name}
+                           onChange={onChangeLinkNameHandler}
+                           placeholder="Link Name"/>
+                    <input type="text"
+                           value={currentLink.value}
+                           onChange={onChangeLinkURLHandler}
+                           placeholder="Link URL"
+                    />
+                    <button disabled={!currentLink.name || !currentLink.value}
+                            className={styles.add}
+                            onClick={onAddHandler}><MdAdd/>
+                    </button>
                 </div>
                 {
                     pageLinks.map((l, i) => {
@@ -29,16 +71,19 @@ export const CreatePage = () => {
                             <div key={i} className={styles.link}>
                                 <div className={styles.linkName}>{l.name}</div>
                                 <div className={styles.linkValue}>{l.value}</div>
-                                <div className={styles.deleteLink}
-                                     onClick={() => onDeleteLinkHandler(l)}><MdClose/>
-                                </div>
+                                <button className={styles.deleteLink}
+                                        onClick={() => onDeleteLinkHandler(l)}><MdClose/>
+                                </button>
                             </div>
                         )
                     })
                 }
             </div>
 
-            <button className={styles.submit}>Create</button>
+            <button disabled={!pageLinks.length || !namePage}
+                    className={styles.submit}
+                    onClick={createPageHandler}>Create
+            </button>
 
 
         </div>
